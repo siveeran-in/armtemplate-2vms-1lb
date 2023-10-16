80ea6d6628172adf5c3eb0615a66e7a481a2cd1f
siva <siveeran.in.ibm.com@gmail.com>
2023-10-16 06:37:39 +0000

    first commit

---
+++ lbtestingwith2vms-1.ps1
@@ -0,0 +1,12 @@
+﻿$projectName = Read-Host -Prompt "Enter a project name with 12 or less letters or numbers that is used to generate Azure resource names"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$adminUserName = Read-Host -Prompt "Enter the virtual machine administrator account name"
+$adminPassword = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
+
+$resourceGroupName = "${projectName}rg"
+$templateUri = "C:\Users\siva_admin\Desktop\devops practice codes\lbtestingwith2vms-1.json"
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -projectName $projectName -location $location -adminUsername $adminUsername -adminPassword $adminPassword
+
+Write-Host "Press [ENTER] to continue."
